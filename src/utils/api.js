86aaf4ddef611728127c4b6c5288984911499123
tyagimayank213 <76656921+tyagimayank213@users.noreply.github.com>
2023-08-28 19:21:57 +0530

    Initial Commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const TMBD_TOKEN= `${import.meta.env.VITE_APP_TMBD_TOKEN}`;
+const TMBD_TOKEN= `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGZlM2ZkMzg5NDBkNzc5MjEwOWY0ZWU0Zjg0NmNlMyIsInN1YiI6IjY0ZWM5OTgyYzYxM2NlMDEyY2M2OTI3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lr72faLX2caZ1pp3j3vrmn_9K4G7jMgczev7AF4h1dA`;
 
 const headers = {
     accept:'application/json',
